e8b432b0648ae923b0ac097dc8aa6fed8930825e
heruiwoniou <heruiwoniou@qq.com>
2023-12-13 16:11:07 +0800

    finish the tranlate api

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-from flask import Flask
-from deep_translator import MyMemoryTranslator as ts
+import os
+
+from flask import Flask, request
+import argparse
+from deep_translator import GoogleTranslator as ts
 
 app = Flask(__name__)
 
+# 使用 argparse 解析命令行参数
+parser = argparse.ArgumentParser(description='Flask App with Proxy Argument')
+parser.add_argument('--proxy', help='Specify the proxy address')
+args = parser.parse_args()
 
-@app.route("/<text>")
-def translate(text):
-    translated = ts(source='zh-CN', target='en-US').translate(text)
+PROXY = args.proxy
+
+
+@app.route("/")
+def home():
+    return "This service is alive."
+
+
+@app.route("/translate", methods=['GET'])
+def translate():
+    text = request.args.get('text')
+    source = request.args.get('from', 'auto')
+    to = request.args.get('to')
+    translated = ts(source=source, target=to, proxies={"http": PROXY, 'https': PROXY} if PROXY else None).translate(text)
     return translated
+
+
+if __name__ == '__main__':
+    app.run()

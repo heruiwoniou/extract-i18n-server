582c502b75eb0bef2a437ba06b00b5031d90e025
heruiwoniou <heruiwoniou@qq.com>
2023-12-14 11:42:21 +0800

    add the docker-compose file

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-python ./main.py --proxy 127.0.0.1:10086
+pip install -r requirements.txt
+
+if [ "$#" -gt 0 ]; then
+    PROXY_ARG="--proxy $1"
+else
+    PROXY_ARG="--proxy 127.0.0.1:10086"
+fi
+
+python ./main.py $PROXY_ARG
